ebd8f37d4decc6425e4df017d977545dcb970001
MarkAbdillaMifsud <markabdilla26@gmail.com>
2023-05-01 16:54:23 +0200

    Simple Scrolling Background

---
+++ Assets/Scripts/LoopingWorld.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoopingWorld : MonoBehaviour
+{
+    public float speed = 4.0f;
+    private Vector3 startPosition;
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.down * speed * Time.deltaTime);
+        if(transform.position.y < -10f)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
