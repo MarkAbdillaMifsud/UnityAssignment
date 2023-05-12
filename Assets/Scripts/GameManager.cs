a1a0394150d461319f312989ff20efa09c519cd2
MarkAbdillaMifsud <markabdilla26@gmail.com>
2023-05-12 16:28:58 +0200

    Added additional UI elements to be fixed

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,8 +7,11 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager manager;
     [Header("Player")]
     public GameObject playerPrefab;
+    public int numOfLives = 3;
+    public int maximumNumOfLives = 6;
     public Transform startingPosition;
     public float spawnSpeed;
     private bool firstTimePlayerSpawned;
@@ -26,11 +29,28 @@ public class GameManager : MonoBehaviour
     [Header("UI")]
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI finalScoreText;
+    public Transform livesPanel;
+    public GameObject lifeIconPrefab;
+    public GameObject blankLifeIconPrefab;
+    private GameObject[] lifeIcons;
+
+    private void Awake()
+    {
+        if(manager != null)
+        {
+            Destroy(this.gameObject);
+        } else
+        {
+            manager = this;
+        }
+    }
 
     private void Start()
     {
         firstTimePlayerSpawned = true;
         enemySpawner.SetActive(false);
+        lifeIcons = new GameObject[maximumNumOfLives];
+        PopulateLifePanel();
         SpawnNewPlayer();
     }
 
@@ -59,6 +79,34 @@ public class GameManager : MonoBehaviour
         player.GetComponent<Player>().SetStartingPosition();
     }
 
+    private void PopulateLifePanel()
+    {
+        float spacing = 50f;
+        Vector2 start = new Vector2(-spacing * (maximumNumOfLives - 1) / 2, 0);
+        for (int i = 0; i < maximumNumOfLives; i++)
+        {
+            if (lifeIcons[i] != null)
+            {
+                Destroy(lifeIcons[i]);
+            }
+        }
+
+        for (int i = 0; i < maximumNumOfLives; i++)
+        {
+            GameObject icon;
+            if (i < numOfLives)
+            {
+                icon = Instantiate(lifeIconPrefab);
+                lifeIcons[i] = icon;
+            }
+            else
+            {
+                icon = Instantiate(blankLifeIconPrefab);
+            }
+            icon.transform.SetParent(livesPanel, false);
+            lifeIcons[i] = icon;
+        }
+    }
     public int GetMaximumEnemyNumber()
     {
         return maxNumOfEnemies;
@@ -87,7 +135,7 @@ public class GameManager : MonoBehaviour
     public void AddToScore(int points)
     {
         finalPointsEarned += points;
-        scoreText.text = "Score " + points;
+        scoreText.text = "Score " + finalPointsEarned;
     }
 
     public void LoadScene(string sceneName)
